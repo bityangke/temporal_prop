83503a8bf6619b86e5f27f95f69b80dcf559472b
Jinwoo Choi <jinchoi@vt.edu>
2016-10-31 20:36:48 -0400

    regression implementation with frcnn 2

--- action_proposal/train_regression_cnn.m
+++ action_proposal/train_regression_cnn.m
@@ -19,6 +19,21 @@ tempPoolingStepSize   = 100;   % Temporal MaxPooling step size (stride)
 shrinkFactor          = tempPoolingStepSize;
 piecewise = true;
 opts.train.gpus = [] ;
+opts.piecewise = true;  % piecewise training (+bbox regression)
+opts.train.gpus = [] ;
+opts.train.batchSize = 1;
+opts.train.numSubBatches = 1 ;
+opts.train.continue = true ;
+opts.train.prefetch = false ; % does not help for two images in a batch
+opts.train.learningRate = 1e-3 / 64 * [ones(1,6) 0.1*ones(1,6)];
+opts.train.weightDecay = 0.0005 ;
+opts.train.numEpochs = 12;
+opts.train.derOutputs = {'losscls', 1, 'lossbbox', 1} ;
+opts.lite = false  ;
+opts.numFetchThreads = 2 ;
+
+opts = vl_argparse(opts, varargin) ;
+display(opts);
 
 % -------------------------------------------------------------------------
 %                                                   Database initialization
@@ -57,73 +72,91 @@ end
 
 num_videos = 1;
 % loop over videos
-for i=1:num_videos
-    fprintf('extracting features from video ... %d/%d\n', i, num_videos);
-    % loop over frames
-    load(fullfile(expDir, '../THUMOS14/vgg16_relu5_features/imagenet-vgg_relu5_on_THUMOS14val_1.mat'));
-%     cnn_feat = cnn_feat_total{1,i};
-    frames = load(imdb.images.path{i});
-    labels = imdb.images.labels{i};
-
-    % grid partitioning in temporal domain of a training video
-%     [starts{i}, durations{i}] = generate_temporal_proposal(frames.im, 100);
-%     [starts{i}, durations{i}] = generate_temporal_proposal2(frames.im); % with various filter sizes and strides
-%     [starts{i}, durations{i}] = generate_temporal_proposal2(cnn_feat); % with various filter sizes and strides
-
-    % match GT labels and proposals
-%     [proposals, targets] = get_training_proposal(labels, starts{i}, durations{i}, 64);
-%     [matched_starts{i}, matched_durations{i}] = match_gt_proposal(labels, starts{i}, durations{i});
-%     proposals.starts = matched_starts{i};
-%     proposals.durations = matched_durations{i};
-%     
-%     % extract CNN features
-%     cnn_feat = {};
-%     for j=1:length(frames.im)
-%         if mod(j,100) == 0
-%             fprintf('features of frame %d/%d\n', j, length(frames.im));
-%         end
-%         im  = single(frames.im{j});
-%         im_ = imresize(im, net.meta.normalization.imageSize(1:2));
-%         im_ = bsxfun(@minus, im_, net.meta.normalization.averageImage) ;
+% for i=1:num_videos
+%     fprintf('extracting features from video ... %d/%d\n', i, num_videos);
+%     % loop over frames
+%     load(fullfile(expDir, '../THUMOS14/vgg16_relu5_features/imagenet-vgg_relu5_on_THUMOS14val_1.mat'));
+% %     cnn_feat = cnn_feat_total{1,i};
+%     frames = load(imdb.images.path{i});
+%     labels = imdb.images.labels{i};
 % 
-%         net.eval({'input', im_});
-%         cnn_feat{j,1} = net.vars(net.getVarIndex('x30')).value;
-%     end
-%     save(fullfile(expDir, sprintf('imagenet-vgg_relu5_on_THUMOS14val_%d.mat',i)), 'cnn_feat','-v7.3');
-
-%     % channel subsampling
-%     cnn_feat_pooled = channel_pooling(cnn_feat);
+%     % grid partitioning in temporal domain of a training video
+% %     [starts{i}, durations{i}] = generate_temporal_proposal(frames.im, 100);
+% %     [starts{i}, durations{i}] = generate_temporal_proposal2(frames.im); % with various filter sizes and strides
+% %     [starts{i}, durations{i}] = generate_temporal_proposal2(cnn_feat); % with various filter sizes and strides
 % 
-%     % temporal max pooling or uniform sampling to generate boxes
-%     temp_pool_cnn_feat = temporal_max_pooling(cnn_feat_pooled, tempPoolingFilterSize, tempPoolingStepSize);    temp_pool_cnn_feat = temporal_max_pooling(cnn_feat, tempPoolingFilterSize, tempPoolingStepSize);
+%     % match GT labels and proposals
+% %     [proposals, targets] = get_training_proposal(labels, starts{i}, durations{i}, 64);
+% %     [matched_starts{i}, matched_durations{i}] = match_gt_proposal(labels, starts{i}, durations{i});
+% %     proposals.starts = matched_starts{i};
+% %     proposals.durations = matched_durations{i};
+% %     
+% %     % extract CNN features
+% %     cnn_feat = {};
+% %     for j=1:length(frames.im)
+% %         if mod(j,100) == 0
+% %             fprintf('features of frame %d/%d\n', j, length(frames.im));
+% %         end
+% %         im  = single(frames.im{j});
+% %         im_ = imresize(im, net.meta.normalization.imageSize(1:2));
+% %         im_ = bsxfun(@minus, im_, net.meta.normalization.averageImage) ;
+% % 
+% %         net.eval({'input', im_});
+% %         cnn_feat{j,1} = net.vars(net.getVarIndex('x30')).value;
+% %     end
+% %     save(fullfile(expDir, sprintf('imagenet-vgg_relu5_on_THUMOS14val_%d.mat',i)), 'cnn_feat','-v7.3');
 % 
-%     % extract features for each temporal proposal
-%     [~, prop_feat_current] = extract_proposal_features(temp_pool_cnn_feat, shrinkFactor, matched_starts{i}, matched_durations{i});
-%     proposal_total_feature = [proposal_total_feature; prop_feat_current];
-%    
-%     % extract regression target values
-%     [ts_current, tl_current] = extract_regression_target_values(labels, shrinkFactor, matched_starts{i}, matched_durations{i});
-%  
-%     targets.ts = ts_current;
-%     targets.tl = tl_current;
-%     
-    inputs = getBatch(opts, imdb, 1);
-end
+% %     % channel subsampling
+% %     cnn_feat_pooled = channel_pooling(cnn_feat);
+% % 
+% %     % temporal max pooling or uniform sampling to generate boxes
+% %     temp_pool_cnn_feat = temporal_max_pooling(cnn_feat_pooled, tempPoolingFilterSize, tempPoolingStepSize);    temp_pool_cnn_feat = temporal_max_pooling(cnn_feat, tempPoolingFilterSize, tempPoolingStepSize);
+% % 
+% %     % extract features for each temporal proposal
+% %     [~, prop_feat_current] = extract_proposal_features(temp_pool_cnn_feat, shrinkFactor, matched_starts{i}, matched_durations{i});
+% %     proposal_total_feature = [proposal_total_feature; prop_feat_current];
+% %    
+% %     % extract regression target values
+% %     [ts_current, tl_current] = extract_regression_target_values(labels, shrinkFactor, matched_starts{i}, matched_durations{i});
+% %  
+% %     targets.ts = ts_current;
+% %     targets.tl = tl_current;
+% %     
+%     inputs = getBatch(opts, imdb, 1);
+% end
 % -------------------------------------------------------------------------
 %                                      Perform Regularized L1 Regression
 % -------------------------------------------------------------------------
-modelPath =  fullfile('..','models','imagenet-vgg-verydeep-16.mat');
-net = apcnn_init('piecewise', piecewise, 'modelPath', modelPath);
+% modelPath =  fullfile('..','models','imagenet-vgg-verydeep-16.mat');
+% net = apcnn_init('piecewise', piecewise, 'modelPath', modelPath);
 
 % Regression Sanity Check!
-if size(proposal_total_feature,2) > size(proposal_total_feature,1)
-    fprintf('The feature matrix does not satisfy this condition!: N > D\n');
-    fprintf('Current N=%d < D=%d\n', size(proposal_total_feature,1), size(proposal_total_feature,2));
-    fprintf('You should collect more data points! \n');
-    return;
-end
-% Regression with Lasso!
+% if size(proposal_total_feature,2) > size(proposal_total_feature,1)
+%     fprintf('The feature matrix does not satisfy this condition!: N > D\n');
+%     fprintf('Current N=%d < D=%d\n', size(proposal_total_feature,1), size(proposal_total_feature,2));
+%     fprintf('You should collect more data points! \n');
+%     return;
+% end
 
+
+% minibatch options
+bopts = net.meta.normalization;
+bopts.useGpu = numel(opts.train.gpus) >  0 ;
+bopts.numFgRoisPerImg = 16;
+bopts.numRoisPerImg = 64;
+bopts.maxScale = 1000;
+bopts.scale = 600;
+bopts.bgLabel = 2;
+bopts.visualize = 0;
+bopts.interpolation = net.meta.normalization.interpolation;
+bopts.numThreads = opts.numFetchThreads;
+bopts.prefetch = opts.train.prefetch;
+
+
+% Regression with Fast R-CNN!
+[net,info] = cnn_train_dag(net, imdb, @(i,b) ...
+                           getBatch(bopts,i,b), ...
+                           opts.train) ;
 % evaluate_regression
 
 % --------------------------------------------------------------------
