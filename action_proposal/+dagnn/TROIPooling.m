e33dc6c462adea9ab204d4ce7f26b2f7b25fe1f3
Jinwoo Choi <jinchoi@vt.edu>
2016-12-08 14:05:35 -0500

    custom STROI pooling forward pass gpu-compatible ing...  20161208

--- action_proposal/+dagnn/TROIPooling.m
+++ action_proposal/+dagnn/TROIPooling.m
@@ -63,23 +63,30 @@ classdef TROIPooling < dagnn.Layer
                 cur_rois = rois([2 4],rois(1,:)==i);
                 sub_sampled = cell(1,size(cur_rois,2));
                 [sub_sampled, randInd] = arrayfun(@(x,y)(obj.get_feature(feature_maps(:,:,:,i), x, y, subdiv(2))), ...
-                                       cur_rois(1,:), cur_rois(2,:), 'UniformOutput', false);
+                                                            cur_rois(1,:), cur_rois(2,:));
+%                 [sub_sampled, randInd] = arrayfun(@(x,y)(obj.get_feature(feature_maps(:,:,:,i), x, y, subdiv(2))), ...
+%                                        cur_rois(1,:), cur_rois(2,:), 'UniformOutput', false);
                 obj.subsample_indice{i} = randInd;
                 %%% max pooling over spatial axis
                 % TO DO: store the max indice
                 pooled = cellfun(@(x)(vl_nnpool(x, [poolsize 1],'pad', [0 1 0 0], 'stride', [stride 1], 'method', 'max')), ...
-                                 sub_sampled, 'UniformOutput', false);
+                                                  sub_sampled, 'UniformOutput', false);
                 cur_output = cat(4,pooled{:});
                 output = cat(4, output, cur_output);
             end
         end
         
+%           function [output] = get_feature(obj, feature_map, point_start, point_end, numGrid)
         function [output, randInd] = get_feature(obj, feature_map, point_start, point_end, numGrid)
             out_feat = feature_map(:, point_start:point_end, :);
             N = size(out_feat,2);
-            randInd = randi(N, [numGrid, 1]);
-            randInd = sort(randInd);
-            output = out_feat(:,randInd,:);
+            selected = randi(N, [numGrid, 1]);
+            selected = sort(selected);  % TO DO: convert this to cell output
+            
+            dim = size(out_feat(:,selected,:));
+            output = mat2cell(out_feat(:,selected,:), dim(1), dim(2), dim(3)); % TO DO: check this is working or not
+            randInd = mat2cell(selected, numGrid, 1);
+%             output = out_feat(:,randInd,:);
         end
         
         function obj = TROIPooling(varargin)
